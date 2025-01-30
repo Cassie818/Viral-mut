896dd0b2b46c894c3af63bcce4eea5b8a3928247
Ruyi Chen <ruyic818@gmail.com>
2025-01-30 15:24:05 +1000

    update plotting function

--- plot/stacked_bar_plot.py
+++ plot/stacked_bar_plot.py
@@ -48,22 +48,22 @@ def plot_distribution_by_mutation_types(
     mutation_types = ['Missense', 'Nonsense', 'Synonymous', 'All']
     effects = ['Pathogenic', 'Likely Pathogenic', 'Benign', 'Likely Benign']
     data = [missense_counts, nonsense_counts, synonymous_counts, combined_counts]
-    colors = ['#d62728', '#ff9896', '#1f77b4', '#aec7e8']  # Colors for each effect
+    colors = ['#c1121f', '#ff9896', '#023e8a', '#aec7e8']  # Colors for each effect
 
     # Transpose data for stacked bar chart
     data_transposed = np.array(data).T
 
     # Plotting stacked bar chart
     x = np.arange(len(mutation_types))  # Position of bars for each mutation type
-    bar_width = 0.6
+    bar_width = 0.5
 
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(8, 6))
     plt.rcParams['font.family'] = 'sans-serif'
     plt.rcParams['font.sans-serif'] = ['arial']
 
     bottom_values = np.zeros(len(mutation_types))
     for i, effect in enumerate(effects):
-        ax.bar(x, data_transposed[i], bar_width, label=effect, color=colors[i], bottom=bottom_values)
+        ax.bar(x, data_transposed[i], bar_width, label=effect, alpha=0.7, color=colors[i], bottom=bottom_values)
         bottom_values += data_transposed[i]
 
     # Format y-axis to include commas for thousands
@@ -72,18 +72,22 @@ def plot_distribution_by_mutation_types(
             return "0"
         return f"{int(x):,}"
 
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_linewidth(1.5)
+
     ax.yaxis.set_major_formatter(FuncFormatter(thousand_separator_formatter))
 
     # Adding labels, title, and legend
     ax.set_xticks(x)
-    ax.set_xticklabels(mutation_types, fontsize=14)
-    ax.tick_params(axis='y', labelsize=14)
+    ax.set_xticklabels(mutation_types, fontsize=16)
+    ax.tick_params(axis='y', labelsize=16)
 
     # Adjusting the legend
-    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.2), ncol=4, fontsize=14, frameon=False)
+    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.3), ncol=2, fontsize=16, frameon=False)
 
     ax.grid(False)
 
     plt.tight_layout(rect=[0, 0.01, 1, 0.95])
     plt.show()
-
