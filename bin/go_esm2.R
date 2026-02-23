cc8bf738ac6e6088ba410ac4e4c8a2827bc83893
Ruyi Chen <ruyic818@gmail.com>
2026-02-23 12:09:37 +1000

    update go_esm2.R

--- bin/go_esm2.R
+++ bin/go_esm2.R
@@ -1,67 +1,75 @@
 library(gprofiler2)
 library(ggplot2)
 library(stringr)
-library(dplyr) 
+library(dplyr)
+library(patchwork)
 
 
-esm_gene_list <- c('ACAD9', 'CACNA1D', 'CAMTA1', 'CC2D2A', 'CD40LG', 
-                   'CFH', 'CPLANE1', 'CYBB', 'DEAF1', 'DICER1', 
-                   'DOK7', 'DYNC1H1', 'DYNC2H1', 'EXT1', 'EYS', 
-                   'GNPTAB', 'GRIN2A', 'GRIN2B', 'HBA1', 'HEXB',
-                   'HLCS', 'IL12RB1', 'IL2RG', 'INF2', 'ITGB4', 
-                   'KCNJ11', 'KCNT1', 'KDM3B', 'KDM6A', 'MAN2B1', 
-                   'MECP2', 'MORC2', 'MSH6', 'MYO15A', 'MYOC', 
-                   'MYRF', 'NBEAL2', 'NEU1', 'NR0B1', 'NSD2', 
-                   'NTRK1', 'OCA2', 'OFD1', 'OTOF', 'PC', 
-                   'PIK3R2', 'PKD1', 'POGZ', 'PURA', 'RAG1', 
-                   'RB1', 'SERPING1', 'SLC16A2', 'SLC4A11', 'SMC1A', 
-                   'SUMF1', 'TAF1', 'TECTA', 'TGM1', 'TNFRSF1A', 
-                   'TP53', 'TYMP', 'USH2A', 'WAS', 'WDR62', 
-                   'WFS1')
+process_gost_data <- function(genes, title_name) {
+  gostres <- gost(query = genes, 
+                  organism = "hsapiens", 
+                  significant = TRUE, 
+                  exclude_iea = TRUE, 
+                  user_threshold = 0.05, 
+                  correction_method = "g_SCS", 
+                  sources = c("GO:MF"))
+  
+  if (is.null(gostres)) return(NULL)
+  
+  df <- gostres$result %>%
+    filter(!term_name %in% c("calcium ion transmembrane transporter activity", 
+                             "glutamate-gated calcium ion channel activity")) %>%
+    arrange(p_value) %>%
+    head(10) %>%
+    mutate(term_name = factor(term_name, levels = rev(unique(term_name[order(intersection_size, -p_value)]))))
+  
+  return(df)
+}
 
-gostres <- gost(query = esm_gene_list, 
-                organism = "hsapiens", 
-                ordered_query = FALSE, 
-                multi_query = FALSE, 
-                significant = TRUE, 
-                exclude_iea = TRUE, 
-                measure_underrepresentation = FALSE, 
-                evcodes = FALSE, 
-                user_threshold = 0.05, 
-                correction_method = "g_SCS", 
-                domain_scope = "annotated", 
-                custom_bg = NULL, 
-                numeric_ns = "", 
-                sources = c("GO:MF")) 
+high_pli_df <- process_gost_data(c('CACNA1D', 'CAMTA1', 'CYBB', 'DICER1', 'DYNC1H1', 
+                                   'EXT1', 'GRIN2A', 'GRIN2B', 'IL2RG', 'INF2', 
+                                   'KDM3B', 'KDM6A', 'MORC2', 'MYRF', 'NR0B1', 
+                                   'OFD1', 'PKD1', 'POGZ', 'PURA', 'RB1', 
+                                   'SERPING1', 'SLC16A2', 'SMC1A', 'TAF1', 'TNFRSF1A', 
+                                   'WAS'), "High")
+
+low_pli_df <- process_gost_data(c('ACAD9', 'CC2D2A', 'DEAF1', 'DOK7', 'DYNC2H1', 
+                                  'EYS', 'GNPTAB', 'HBA1', 'HEXB', 'HLCS', 
+                                  'IL12RB1', 'ITGB4', 'KCNJ11', 'KCNT1', 'MAN2B1', 
+                                  'MSH6', 'MYO15A', 'MYOC', 'NBEAL2', 'NEU1', 
+                                  'NTRK1', 'OCA2', 'OTOF', 'PC', 'PIK3R2', 
+                                  'RAG1', 'SLC4A11', 'SUMF1', 'TECTA', 'TGM1', 
+                                  'TYMP', 'USH2A', 'WDR62', 'WFS1'), "Low")
 
 
-res_df <- gostres$result
-res_df <- res_df %>%
-  filter(term_name != "protein dimerization activity") %>%      
-  filter(term_name != "voltage-gated channel activity") %>%
-  filter(p_value < 0.05) 
+build_plot <- function(data, title, color_low = "#542788") {
+  ggplot(data, aes(x = intersection_size, y = term_name, fill = p_value)) +
+    geom_col(width = 0.75) +
+    scale_y_discrete(labels = function(x) str_wrap(x, width = 40)) + 
+    scale_fill_gradient(low = color_low, high = "#F0F0F0", 
+                        name = "p-value",
+                        labels = function(x) sprintf("%.3f", x),
+                        guide = guide_colorbar(barwidth = 0.8, barheight = 4)) +
+    labs(title = title, x = "Count", y = NULL) +
+    theme_classic() +
+    theme(
+      plot.title = element_text(size = 13, hjust = 0.5),
+      axis.text.y = element_text(size = 11, color = "black", lineheight = 0.8),
+      axis.text.x = element_text(size = 11),
+      axis.line.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      panel.grid.major.x = element_line(color = "grey95"),
+      plot.margin = margin(5, 5, 5, 5)
+    )
+}
 
-res_df <- res_df[order(res_df$p_value), ]
-top_terms <- head(res_df, 10) 
 
-top_terms$term_name <- factor(top_terms$term_name, 
-                              levels = top_terms$term_name[order(top_terms$intersection_size)])
+p_high <- build_plot(high_pli_df, "PLM better (high pLI)", color_low = "#4D004B")
+p_low <- build_plot(low_pli_df, "PLM better (low pLI)", color_low = "#084594")
 
-ggplot(top_terms, aes(x = intersection_size, y = term_name, fill = p_value)) +
-  geom_col() +
-  scale_y_discrete(labels = function(x) str_wrap(x, width = 40)) +
-  scale_fill_gradient(low = "purple", high = "#E0E0E0", name = "p_value")+
-  labs(title = "PLM better",
-       x = "Count", 
-       y = NULL) +
-  theme_classic() +
-  theme(
-    panel.grid.major.x = element_line(color = "grey90"), 
-    axis.line.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    aspect.ratio = 1.6,
-    plot.title = element_text(
-      size = 12,  
-      hjust = 0.5,    
-    ),
-  )
+
+final_plot <- (p_high / p_low) + 
+  plot_layout(guides = "collect") & 
+  theme(legend.position = "right")
+
+print(final_plot)
